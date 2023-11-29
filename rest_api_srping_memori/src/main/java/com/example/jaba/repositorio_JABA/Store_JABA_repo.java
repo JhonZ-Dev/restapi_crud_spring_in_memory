f742f18c2e251bc513c9ed8c83a746cf0218a7c0
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-11-28 21:04:55 -0500

    Update Store_JABA_repo.java

--- rest_api_srping_memori/src/main/java/com/example/jaba/repositorio_JABA/Store_JABA_repo.java
+++ rest_api_srping_memori/src/main/java/com/example/jaba/repositorio_JABA/Store_JABA_repo.java
@@ -1,7 +1,14 @@
 package com.example.jaba.repositorio_JABA;
 
+import com.example.jaba.model_JABA.Store_JABA;
 import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Repository
 public class Store_JABA_repo {
+    private final List<Store_JABA> store_jabaList = new ArrayList<>();
+    
+
 }
