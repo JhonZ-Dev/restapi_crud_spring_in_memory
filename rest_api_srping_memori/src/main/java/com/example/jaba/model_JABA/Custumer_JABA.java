d9e76eb5da70aaadee5bc3875dbdc815606dd165
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-11-28 20:43:29 -0500

    Creación Proyecto

---
+++ rest_api_srping_memori/src/main/java/com/example/jaba/model_JABA/Custumer_JABA.java
@@ -0,0 +1,10 @@
+package com.example.jaba.model_JABA;
+
+import lombok.Data;
+
+@Data
+public class Custumer_JABA {
+    private int customerId;
+    private String fullName;
+
+}
---
+++ rest_api_srping_memori/src/main/java/com/example/jaba/model_JABA/Store_JABA.java
@@ -0,0 +1,10 @@
+package com.example.jaba.model_JABA;
+
+import lombok.Data;
+
+@Data
+public class Store_JABA {
+    private int storeId;
+    private String storeName;
+
+}
