4c0d073cdf43d9c22969e3c825c3e5ffa9a8f87f
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-11-28 21:05:36 -0500

    Update Store_JABA_repo.java

--- rest_api_srping_memori/src/main/java/com/example/jaba/repositorio_JABA/Store_JABA_repo.java
+++ rest_api_srping_memori/src/main/java/com/example/jaba/repositorio_JABA/Store_JABA_repo.java
@@ -9,6 +9,12 @@ import java.util.List;
 @Repository
 public class Store_JABA_repo {
     private final List<Store_JABA> store_jabaList = new ArrayList<>();
+    // Create
+    public Store_JABA save(Store_JABA store) {
+        store_jabaList.add(store);
+        return store;
+    }
+
     
 
 }
