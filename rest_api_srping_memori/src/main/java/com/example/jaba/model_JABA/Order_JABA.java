95a43bd8a2e1816a169ffb794b6e7495f51f3a47
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-11-28 20:51:16 -0500

    Update Order_JABA.java

--- rest_api_srping_memori/src/main/java/com/example/jaba/model_JABA/Order_JABA.java
+++ rest_api_srping_memori/src/main/java/com/example/jaba/model_JABA/Order_JABA.java
@@ -2,6 +2,21 @@ package com.example.jaba.model_JABA;
 
 import lombok.Data;
 
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
 @Data
 public class Order_JABA {
+    private Integer OrderId;
+
+    /*Relacion con custumer*/
+
+
+    /*Relacion con Store*/
+
+    /*otros atributos*/
+    private Integer cantidadventas;
+    private LocalDateTime date;
+    private LocalTime tiempo;
+
 }
